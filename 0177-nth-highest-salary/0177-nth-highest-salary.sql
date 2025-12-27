001017abf05dbddf1f6786feb096c05ab24f405b
Varshitha <velamalavarshitha117@gmail.com>
2025-12-27 17:50:40 +0530

    0177-NTH-HIGHEST-SALARY Stats: Time: 370 ms (97.79%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,8 +1,8 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-    DECLARE X INT DEFAULT N-1;
+    SET N=N-1;
   RETURN (
       # Write your MySQL query statement below.
-        SELECT  distinct salary  from Employee order by salary desc limit 1 offset X
+        SELECT  distinct salary  from Employee order by salary desc limit 1 offset N
   );
 END
